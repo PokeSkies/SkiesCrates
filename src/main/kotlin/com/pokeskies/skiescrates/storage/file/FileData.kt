c68f1653caf0271828d898c45b41cf690a1ff8a8
Stampede <thestampede2011@gmail.com>
2025-09-10 11:55:42 -0500

    Switched JSON file storage to concurrent hashmap

--- src/main/kotlin/com/pokeskies/skiescrates/storage/file/FileData.kt
+++ src/main/kotlin/com/pokeskies/skiescrates/storage/file/FileData.kt
@@ -2,9 +2,10 @@ package com.pokeskies.skiescrates.storage.file
 
 import com.pokeskies.skiescrates.data.userdata.UserData
 import java.util.*
+import java.util.concurrent.ConcurrentHashMap
 
 class FileData {
-    var userdata: HashMap<UUID, UserData> = HashMap()
+    var userdata: MutableMap<UUID, UserData> = ConcurrentHashMap()
     override fun toString(): String {
         return "FileData(userdata=$userdata)"
     }
